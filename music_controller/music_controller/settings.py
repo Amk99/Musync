1f7fd24d7c6168d137edf02d7fdb7becffc33c4b
atharva kotibhaskar <atharvatamk@gmail.com>
2025-01-25 12:34:45 +0530

    deleted env and added gitignore

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-kgd4d0ur@#!m62d7xknt=dh&zw_6e&(^$@_9)c!!1i2^9p!*qn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
