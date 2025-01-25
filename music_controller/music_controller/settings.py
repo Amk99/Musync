bff1973a611dd83afb685648b84f49f14606af2c
atharva kotibhaskar <atharvatamk@gmail.com>
2025-01-25 13:06:21 +0530

    updated settings

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-SECRET_KEY = 'django-insecure-kgd4d0ur@#!m62d7xknt=dh&zw_6e&(^$@_9)c!!1i2^9p!*qn'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
